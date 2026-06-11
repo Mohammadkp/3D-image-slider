c2a717e97813d0a21963a5de3ac66095e836f068
Mohammadkp <forcourselogin@gmail.com>
2026-06-11 15:13:58 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,62 +1,55 @@
-
 var radius = 350; // how big of the radius
-var autoRotate = true; // auto rotate or not
-var rotateSpeed = -60; // unit: seconds/360 degrees
-var imgWidth = 250; // width of images (unit: px)
-var imgHeight = 350; // height of images (unit: px)
-let isPinching = false;
+var autoRotate = true;
+var rotateSpeed = -60;
+var imgWidth = 250;
+var imgHeight = 350;
 
 if (window.innerWidth <= 768) {
     radius = 350;
     imgWidth = 180;
     imgHeight = 250;
 }
-// Link of background music - set 'null' if you dont want to play background music
-var bgMusicURL = 'https://api.soundcloud.com/tracks/143041228/stream?client_id=587aa2d384f7333a886010d5f52f302a';
-var bgMusicControls = true; // Show UI music control
 
-
-
-// ===================== start =======================
-// animation start after 1000 miliseconds
+// animation start after 1000 milliseconds
 setTimeout(init, 1000);
 
 var odrag = document.getElementById('drag-container');
 var ospin = document.getElementById('spin-container');
 var aImg = ospin.getElementsByTagName('img');
-var aEle = [...aImg]; // combine 2 arrays
+var aEle = [...aImg];
 
 // Size of images
 ospin.style.width = imgWidth + "px";
 ospin.style.height = imgHeight + "px";
 
-// Size of ground - depend on radius
+// Size of ground
 var ground = document.getElementById('ground');
 ground.style.width = radius * 3 + "px";
 ground.style.height = radius * 3 + "px";
 
 function init(delayTime) {
   for (var i = 0; i < aEle.length; i++) {
-    aEle[i].style.transform = "rotateY(" + (i * (360 / aEle.length)) + "deg) translateZ(" + radius + "px)";
+    aEle[i].style.transform =
+      "rotateY(" + (i * (360 / aEle.length)) + "deg) translateZ(" + radius + "px)";
     aEle[i].style.transition = "transform 1s";
-    aEle[i].style.transitionDelay = delayTime || (aEle.length - i) / 4 + "s";
+    aEle[i].style.transitionDelay =
+      delayTime || (aEle.length - i) / 4 + "s";
   }
 }
 
 function applyTranform(obj) {
-  // Constrain the angle of camera (between 0 and 180)
-  if(tY > 180) tY = 180;
-  if(tY < 0) tY = 0;
+  if (tY > 180) tY = 180;
+  if (tY < 0) tY = 0;
 
-  // Apply the angle
-  obj.style.transform = "rotateX(" + (-tY) + "deg) rotateY(" + (tX) + "deg)";
+  obj.style.transform =
+    "rotateX(" + (-tY) + "deg) rotateY(" + (tX) + "deg)";
 }
 
 function playSpin(yes) {
-  ospin.style.animationPlayState = (yes?'running':'paused');
+  ospin.style.animationPlayState = (yes ? 'running' : 'paused');
 }
 
-var sX, sY, nX, nY, desX = 0,
+var desX = 0,
     desY = 0,
     tX = 0,
     tY = 10;
@@ -64,107 +57,87 @@ var sX, sY, nX, nY, desX = 0,
 // auto spin
 if (autoRotate) {
   var animationName = (rotateSpeed > 0 ? 'spin' : 'spinRevert');
-  ospin.style.animation = `${animationName} ${Math.abs(rotateSpeed)}s infinite linear`;
+  ospin.style.animation =
+    `${animationName} ${Math.abs(rotateSpeed)}s infinite linear`;
 }
 
-// add background music
-if (bgMusicURL) {
-  document.getElementById('music-container').innerHTML += `
-<audio src="${bgMusicURL}" ${bgMusicControls? 'controls': ''} autoplay loop>    
-<p>If you are reading this, it is because your browser does not support the audio element.</p>
-</audio>
-`;
-}
-
-// setup events
+// Mouse / Touch Drag Rotate
 document.onpointerdown = function (e) {
 
-  if (e.pointerType === "touch" && e.isPrimary) 
-return;
-  
   clearInterval(odrag.timer);
+
   e = e || window.event;
+
   var sX = e.clientX,
       sY = e.clientY;
 
   this.onpointermove = function (e) {
+
     e = e || window.event;
+
     var nX = e.clientX,
         nY = e.clientY;
+
     desX = nX - sX;
     desY = nY - sY;
+
     tX += desX * 0.1;
     tY += desY * 0.1;
+
     applyTranform(odrag);
+
     sX = nX;
     sY = nY;
   };
 
-  this.onpointerup = function (e) {
+  this.onpointerup = function () {
+
     odrag.timer = setInterval(function () {
+
       desX *= 0.95;
       desY *= 0.95;
+
       tX += desX * 0.1;
       tY += desY * 0.1;
+
       applyTranform(odrag);
+
       playSpin(false);
+
       if (Math.abs(desX) < 0.5 && Math.abs(desY) < 0.5) {
         clearInterval(odrag.timer);
         playSpin(true);
       }
+
     }, 17);
-    this.onpointermove = this.onpointerup = null;
+
+    this.onpointermove = null;
+    this.onpointerup = null;
   };
 
   return false;
 };
 
-document.onmousewheel = function(e) {
+// Mouse wheel zoom (PC)
+document.onmousewheel = function (e) {
   e = e || window.event;
+
   var d = e.wheelDelta / 20 || -e.detail;
+
   radius += d;
+
   init(1);
 };
 
+// Music
 const music = document.getElementById("bgMusic");
 
 function startMusic() {
-    music.play().catch(err => console.log(err));
+  if (music) {
+    music.play().catch(() => {});
+  }
 }
 
 ["pointerdown", "touchstart", "click", "keydown"].forEach(event => {
-    document.addEventListener(event, startMusic, { once: true });
+  document.addEventListener(event, startMusic, { once: true });
 });
-
-let initialDistance = 0;
-
-document.addEventListener("touchstart", function (e) {
-    if (e.touches.length === 2) {
-        initialDistance = Math.hypot(
-            e.touches[0].clientX - e.touches[1].clientX,
-            e.touches[0].clientY - e.touches[1].clientY
-        );
-    }
-});
-
-document.addEventListener("touchmove", function (e) {
-    if (e.touches.length === 2) {
-        e.preventDefault();
-
-        let currentDistance = Math.hypot(
-            e.touches[0].clientX - e.touches[1].clientX,
-            e.touches[0].clientY - e.touches[1].clientY
-        );
-
-        let delta = (currentDistance - initialDistance) * 2;
-
-        radius -= delta;
-
-        if (radius < 400) radius = 400;
-       // if (radius > 600) radius = 600;
-
-        init(0);
-
-        initialDistance = currentDistance;
-    }
-}, { passive: false });
